57d1db5a1a85a14b3603243ac7208c0d33c38ccc
DhruvBacancy <dhruv.patel@bacancy.com>
2023-10-12 10:50:09 +0530

    User onboarding component and UI created

---
+++ src/components/user entry/Register.jsx
@@ -0,0 +1,101 @@
+import React from "react";
+import { Avatar, Grid, Paper, TextField, Typography } from "@mui/material";
+import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Button from "@mui/material/Button";
+import { styled } from "@mui/material/styles";
+import CloudUploadIcon from "@mui/icons-material/CloudUpload";
+import "../../align.css";
+
+const Register = () => {
+  const paperStyle = { padding: "30px 20px", width: 300, margin: "20px auto" };
+  const headerStyle = { margin: 0 };
+  const avatarStyle = { backgroundColor: "#1bbd7e" };
+  const VisuallyHiddenInput = styled("input")({
+    clip: "rect(0 0 0 0)",
+    clipPath: "inset(50%)",
+    height: 1,
+    overflow: "hidden",
+    position: "absolute",
+    bottom: 0,
+    left: 0,
+    whiteSpace: "nowrap",
+    width: 1,
+  });
+  return (
+    <Grid>
+      <Paper elevation={20} style={paperStyle}>
+        <Grid align="center">
+          <Avatar style={avatarStyle}>
+            <AddCircleOutlineIcon />
+          </Avatar>
+          <h2 style={headerStyle}>Sign Up</h2>
+          <Typography variant="caption" gutterBottom>
+            Please fill this form to create an account !
+          </Typography>
+        </Grid>
+        <form>
+          <TextField
+            fullWidth
+            label="First Name"
+            placeholder="Enter your first name"
+            required
+          />
+          <TextField
+            fullWidth
+            label="Last Name"
+            placeholder="Enter your last name"
+          />
+          <TextField
+            fullWidth
+            label="Email"
+            placeholder="Enter your email"
+            required
+          />
+          <TextField
+            fullWidth
+            label="Password"
+            placeholder="Enter your password"
+            required
+          />
+          <TextField
+            fullWidth
+            label="Confirm Password"
+            placeholder="Confirm your password"
+            required
+          />
+          <FormControlLabel
+            control={<Checkbox name="checkedA" />}
+            label="I accept the terms and conditions."
+          />
+          <div className="align-center">
+            <Button
+              component="label"
+              variant="contained"
+              startIcon={<CloudUploadIcon />}
+            >
+              Profile Picture (optional)
+              <VisuallyHiddenInput type="file" />
+            </Button>
+          </div>
+          <br />
+          <div>
+            <div className="align-left">
+              <Button type="submit" variant="contained" color="primary">
+                Sign up
+              </Button>
+              <div className="align-right">
+                <Button type="submit" variant="contained" color="primary">
+                  Back
+                </Button>
+              </div>
+            </div>
+          </div>
+        </form>
+      </Paper>
+    </Grid>
+  );
+};
+
+export default Register;
